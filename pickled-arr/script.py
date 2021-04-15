92fee51d5e40c809c374f7544f154edee3ff2bac
Naman Tuli <namantuli2000@gmail.com>
2021-04-16 02:40:19 +0530

    1/4

--- pickled-arr/script.py
+++ pickled-arr/script.py
@@ -3,17 +3,22 @@ import os
 import csv
 import pickle
 
-files=os.listdir("slider")
-data={}
-data['filename']=[]
-data['val']=[]
-for count,file in enumerate(files):
-    with open(os.path.join("slider",file),"rb") as f:
-        #print(os.path.join("slider",file))
-        try:
-            arr=pickle.load(f)
-            data['filename'].append(file)
-            data['val'].append(arr)
-        except:pass
-df=pd.DataFrame.from_dict(data)
-df.to_csv("slider.csv",index=False)
+# files=os.listdir("slider")
+# data={}
+# data['filename']=[]
+# data['val']=[]
+# for count,file in enumerate(files):
+#     with open(os.path.join("slider",file),"rb") as f:
+#         #print(os.path.join("slider",file))
+#         try:
+#             arr=pickle.load(f)
+#             data['filename'].append(file)
+#             data['val'].append(arr)
+#         except:pass
+# df=pd.DataFrame.from_dict(data)
+# df.to_csv("slider.csv",index=False)
+
+with open("slider/U_mfcc.pb",'rb') as f:
+	data=pickle.load(f)
+
+print(data)
