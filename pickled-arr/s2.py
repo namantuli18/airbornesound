7108a30652b8076d55ccf651da1999378b997258
Prakhar Singh <prakharsingh13@gmail.com>
2021-04-16 04:05:24 +0530

    scatter plot fixes

--- pickled-arr/s2.py
+++ pickled-arr/s2.py
@@ -6,13 +6,12 @@ import pandas as pd
 import json
 import numpy
 data={}
-with open(os.path.join("slider",'U_mfcc.pb'),"rb") as f:
+with open(os.path.join("slider",'Y.pb'),"rb") as f:
     try:
         arr=pickle.load(f)
         print(arr[3])
         data['values']= arr
         print(data['values'])
     except:pass
-numpy.savetxt("foo.csv", arr, delimiter=",")
-df=pd.DataFrame.from_dict(data)
-pd.DataFrame(arr).to_csv("slider.csv")
+numpy.savetxt("U_vggish_Y.csv", arr, delimiter=",")
+
